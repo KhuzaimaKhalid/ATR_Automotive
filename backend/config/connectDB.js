5c4993141331dc3dfaba076e1575e39c13deda3f
Khuzaima Khalid <khuzaimakhalidkhan@gmail.com>
2026-07-23 19:14:12 +0500

    day 17 added

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -1,7 +1,20 @@
 const Database = require("better-sqlite3");
 const path = require("path");
+const fs = require("fs");
 
-const db = new Database(path.join(__dirname, "../database.db"));
+let dbPath = path.join(__dirname, "../database.db");
+
+if (process.env.VERCEL) {
+    const tmpPath = path.join("/tmp", "database.db");
+    
+    if (!fs.existsSync(tmpPath)) {
+        fs.copyFileSync(dbPath, tmpPath);
+    }
+    
+    dbPath = tmpPath;
+}
+
+const db = new Database(dbPath);
 
 try {
     db.prepare("SELECT 1").get();
